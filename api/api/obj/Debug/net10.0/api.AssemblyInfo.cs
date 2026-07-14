7bd692aab4f26eb3699b32ad811b1a1c0e14af03
Felipe Rodriguez <luismatarrita64@gmail.com>
2026-07-14 02:41:09 -0600

    Agrego ...

--- api/api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+710e7a6f8b9cc577e19b96f295638059894e7eec")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
